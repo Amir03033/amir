af4b89a1e6a90f1fab2a781b9ae1b714c27a2187
amir <Snapzakaria69@gmail.com>
2026-09-19 11:42:51 +0200

    responsive

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AmirPanelProvider::class,
+//    App\Providers\Filament\AmirPanelProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
 ];
