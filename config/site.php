e9759f04e32f43a134fc78ccc6ebbed6e2b66f49
amir <Snapzakaria69@gmail.com>
2026-09-19 12:14:05 +0200

    responsive

--- config/site.php
+++ config/site.php
@@ -3,7 +3,7 @@
 return [
     'name' => 'Amir Jebbari',
     'email' => env('SITE_EMAIL', 'hosseinjebbari@gmail.com'),
-    'phone' => env('SITE_PHONE', '+31 6 13101525'),
+    'phone' => env('SITE_PHONE', '+31 6 12121212'),
     'phone_href' => env('SITE_PHONE_HREF', '+31612121212'),
     'location' => 'Overijsel',
     'linkedin' => 'https://www.linkedin.com/in/amir-jebbari-19a63b330/',
